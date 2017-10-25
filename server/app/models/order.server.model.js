fbb8524f8827215b88623ea05c5a9749dbfa4b99
Unknown <josuejacobstercero@gmail.com>
2017-10-25 15:21:53 -0600

    asdf

--- server/app/models/order.server.model.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-const OrderSchema = new Schema({
-
-	user: String,
-	item:
-});
--- server/config/mongoose.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-module.exports = function() {
-	const db = mongoose.connect('mongodb://pro:foodie@ds040027.mlab.com:40027/foodie');
-
-	require('../app/models/user.server.model');
-	
-	return db;
-}
