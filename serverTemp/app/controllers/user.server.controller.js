76986bdb6a6df94bca0fb00e2be5220216d590bb
ciborg245 <alejandrochgu@hotmail.com>
2017-10-26 18:54:48 -0600

    servidor temporal

---
+++ serverTemp/app/controllers/user.server.controller.js
@@ -0,0 +1,18 @@
+const User = require('mongoose').model('User');
+const passport = require('passport');
+
+exports.signup = function(req, res, next) {
+	const user = new User(req.body);
+	user.provider = 'local';
+
+	console.log(user);
+
+	user.save((err) => {
+		if (err) {
+			res.send(err);
+		} else {
+			console.log("creado perro");
+			res.json({message: "El usuario ha sido creado."});
+		}
+	});
+};
